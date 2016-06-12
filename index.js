ec44427f29c011c82b3d05e8942c66d0b5455597
sunsplat <heeycutie@yahoo.com>
2016-06-12 16:50:35 -0400

    adding geolocation

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ function success(position) {
       Geo.lng = position.coords.longitude;
 }
 
+var output = document.getElementById("out");
+output.innerHTML = '<p>Latitude is ' + Geo.lat + '° <br>Longitude is ' + Geo.lng + '°</p>';
+
 var key = 'ede211c9afc45826594bb0936d9d5b9c';
+
 var City = 'Decatur';
 var Weather = "http://api.openweathermap.org/data/2.5/weather?q=" + City + "&APPID=" + key;
 
@@ -22,20 +26,34 @@ $.ajax({
 	url : Weather,
 	dataType : "jsonp",
 	success : function(data) {
-	// get all the information
+		var location =data['sys']['city'];
+		var temp = data['main']['temp'];
+		var img = data['weather']['icon'];
+		var desc = data['weather']['description'];
+		var wind = data['wind']['speed'];
 	}
 });
 
-var location =data['location']['city'];
-var temp = data['current_observation']['temp_f'];
-var img = data['current_observation']['icon_url'];
-var desc = data['current_observation']['weather'];
-var wind = data['current_observation']['wind_string'];
 
-//setting the spans to the correct parameters
-$('#location').html(location);
-$('#temp').html(temp);
-$('#desc').html(desc);
-$('#wind').html(wind);
-//filling the image src attribute with the image url
-$('#img').attr('src', img);
+function toggleTempMeasurement() {
+	var current_choice = document.getElementById('temp_measure')
+	if (current_choice.innerHTML == "&#8451;") {
+		current_choice.innerHTML = "&#8457;";
+		console.log("&#8457;");
+	} else {
+		current_choice.innerHTML = "&#8451;";
+		console.log("&#8451;");
+	}
+}
+
+
+
+
+
+// //setting the spans to the correct parameters
+// $('#location').html(location);
+// $('#temp').html(temp);
+// $('#desc').html(desc);
+// $('#wind').html(wind);
+// //filling the image src attribute with the image url
+// $('#img').attr('src', img);
