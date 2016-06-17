e78e9979a58f0be8210c68b3ad6ea21c924c621b
sunsplat <heeycutie@yahoo.com>
2016-06-17 17:44:11 -0400

    imported codepen code

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
+
 var OpenWeatherKey = 'ede211c9afc45826594bb0936d9d5b9c';
 
-var City = 'http://freegeoip.net/json/';
+var cityUrl = 'http://freegeoip.net/json/';
 
-function getLocation() {
-		$.ajax({
-			url : City,
-			dataType : "json",
-			success : function(data) {
-				var country =data['country_code'];
-				var region = data['region_code'];
-				var city = data['city'];
-				var latitude = data['latitude'];
-				var longitude = data['longitude'];
-				$('#location').html(city + ', ' + region);
-				var Weather = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=" + OpenWeatherKey;
-				getWeather(Weather);
-			}
-		});
-}
 
 function getWeather(url) {
 	$.ajax({
@@ -27,16 +12,52 @@ function getWeather(url) {
 			var temp = data['main']['temp'];
 			var img = data['weather']['icon'];
 			var desc = data['weather']['description'];
-			var wind = data['wind']['speed'];
+			var rain = data['weather']['main'];
+      var sunset = new Date((data['sys']['sunset'])*1000);
+      sunset = sunset.toString().slice(16,21);
+      //var sunsetOffset = sunset.getTimezoneOffset();
+      // sunset = sunset.toGMTString() +  "<br>"+sunset.toLocaleString());
+      //sunset = sunset.toLocaleString();
+      //var x = new Date();
+      //var date = new Date(x.getTime() - x.getTimezoneOffset() * 60000).toJSON().slice(0,10);
+      //var time = new Date(x.getTime() - x.getTimezoneOffset() * 60000).toJSON().slice(11,16);
 			$('#temp').html(temp);
 			$('#desc').html(desc);
-			$('#wind').html(wind);
+			$('#wind').html(rain);
+      $('#sunset').html(sunset + " ");
+      //$('#test').html(date + " " + time);
 		}
 	});
 }
 
 var tempMeasure = true;
+
+function getLocation() {
+		$.ajax({
+			url : cityUrl,
+			dataType : "json",
+			success : function(data) {
+				if (tempMeasure == true) {
+					var tempUnit = "imperial";
+				} else {
+					tempUnit = "metric";
+				}
+
+				var country =data['country_code'];
+				var region = data['region_code'];
+				var city = data['city'];
+				var latitude = data['latitude'];
+				var longitude = data['longitude'];
+				$('#location').html(city + ', ' + region);
+				var Weather = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=" + tempUnit + "&APPID=" + OpenWeatherKey;
+				getWeather(Weather);
+				console.log(Weather);
+			}
+		});
+}
+
 function toggleTempMeasurement() {
+	getLocation();
 	if (tempMeasure == false) {
 		current_choice.innerHTML = "&#8457;";
 		tempMeasure = true;
@@ -47,6 +68,3 @@ function toggleTempMeasurement() {
 }
 
 getLocation();
-
-// //filling the image src attribute with the image url
-// $('#img').attr('src', img);
